0e0377655b92d418e856db29cc867eed569fc257
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-17 19:12:03 +0600

    Service Package Added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,31 @@ import Banner from "./Banner";
 import "./Home.css";
 import Service from "./Service";
 
+const pack = [
+  {
+    id: 1,
+    title: "Classic Photo & Video",
+    price: 245,
+    img: "https://weddingdiary.com.bd/wp-content/uploads/2021/08/8J5A2509-370x270.jpg",
+    dec: "1 Top Photographer, 4 Hours Shoot, Portrait Light Setup, Numbers of Photos: Unlimited (All Post Processed),Print: 4R (4x6): 100 Copies & 12L (12X18): 1 Copy, All Printed Photos Delivered With Matte/Glossy Paper. Video Quality: Full HD 1080p (1920X1080) With Blu-Ray Disc, 20-50 Minutes Edited Cinematography With Your Selected Songs",
+  },
+  {
+    id: 2,
+
+    title: "Expressions Photo & Video",
+    price: 365,
+    img: "https://weddingdiary.com.bd/wp-content/uploads/2021/08/12-370x270.jpg",
+    dec: "A Certified Videographer for 8 Hours,30 Page, 10X10 Wedding Photo Album, Unlimited Photos & Video Captured All Edited High-Resolution Copyright Released Digital Photo Files",
+  },
+  {
+    id: 3,
+    title: "Storytelling Photo & Video",
+    price: 445,
+    img: "https://weddingdiary.com.bd/wp-content/uploads/2021/08/BS9A8233..-370x270.jpg",
+    dec: "A Certified Photographer for 10 Hours,Second Certified Photographer for 8 Hours, Unlimited Coverage Locations, Unlimited Photos & Video Captured ",
+  },
+];
+
 const Home = () => {
   return (
     <>
@@ -10,7 +35,14 @@ const Home = () => {
         <Banner />
       </div>
       <div className="container">
-        <Service />
+        <div className="tittle">
+          <h1>Photography / Videography Service</h1>
+        </div>
+        <div className="service">
+          {pack.map((service) => (
+            <Service key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/pages/Home/Service.js
+++ src/pages/Home/Service.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./Service.css";
 
-const Service = () => {
+const Service = ({ service }) => {
+  const { img, title, price, dec } = service;
   return (
-    <div>
-      <h2>This is Service Page</h2>
-    </div>
+    <>
+      <div className="service_wrapper">
+        <div className="content">
+          <img src={img} alt="" />
+          <div className="title_price">
+            <h2> {title} </h2>
+            <h2>$ {price} </h2>
+          </div>
+
+          <p>{dec.slice(0, 150)}</p>
+          <button>Explore Package</button>
+        </div>
+      </div>
+    </>
   );
 };
 
