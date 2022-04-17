0465289ed87ce2701caa08e940ea86ed72a38446
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-17 21:36:53 +0600

    login and signup page Done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Login.css";
+import { AiFillGoogleSquare } from "react-icons/ai";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Login = () => {
   return (
@@ -7,17 +9,29 @@ const Login = () => {
       <div className="login_wrapper">
         <div className="login_content">
           <h1>Sign in</h1>
+          <p>
+            To Keep connected with us. Please login your personal information
+          </p>
           <form>
-            <div className="email">
+            <div className="email common">
               <p>Email</p>
               <input type="email" placeholder="Enter your Email" />
             </div>
-            <div className="Password">
-              <p>Email</p>
+            <div className="password common">
+              <p>Password</p>
               <input type="password" placeholder="Enter your Password" />
             </div>
             <button type="submit">Sign in</button>
           </form>
+
+          <div className="social__login">
+            <i>
+              <AiFillGoogleSquare />
+            </i>
+            <i>
+              <FaGithubSquare />
+            </i>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div>
-      <p>Sign up PAges</p>
+    <div className="login container">
+      <div className="login_wrapper">
+        <div className="login_content">
+          <h1>Create an Account</h1>
+          <p>
+            To Keep connected with us. Create and Account with your personal
+            information
+          </p>
+          <form>
+            <div className="email common">
+              <p>Email</p>
+              <input type="email" placeholder="Enter your Email" />
+            </div>
+            <div className="password common">
+              <p>Password</p>
+              <input type="password" placeholder="Enter your Password" />
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
